3ed720e092678ba76370c557e0f5ac6bae699238
Matthew Riley <mdriley@gmail.com>
2018-03-06 00:48:05 -0800

    Demo changes

--- cmd/travisci/main.go
+++ cmd/travisci/main.go
@@ -17,11 +17,28 @@ func main() {
 
 	fmt.Printf("%+v\n", client)
 
-	user, err := client.CurrentUser()
+	repos, err := client.RepositoriesForCurrentUser()
 	if err != nil {
 		fmt.Printf("ERROR: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("%+v\n", user)
+	fmt.Printf("%+v\n", repos.Pagination.First)
+	fmt.Printf("%+v\n", repos.Pagination.Next)
+	fmt.Printf("%+v\n", repos.Pagination.Last)
+
+	for _, repo := range repos.Repositories {
+		fmt.Printf("%s\n", repo.Slug)
+	}
+
+	// fmt.Printf("%+v\n", repos.Repositories)
+	// fmt.Printf("%+v\n", repos.Pagination)
+
+	envvars, err := client.EnvVarsForRepository("pulumi/pulumi")
+	if err != nil {
+		fmt.Printf("ERROR: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%+v\n", envvars)
 }
